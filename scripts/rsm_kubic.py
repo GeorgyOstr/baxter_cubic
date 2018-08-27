332603668f95a6f329c511a4161efc3af7d70c9b
georgy <georgyostr@gmail.com>
2018-08-27 02:54:12 -0700

    Not debugged rotations

--- scripts/rsm_kubic.py
+++ scripts/rsm_kubic.py
@@ -2,11 +2,12 @@
 
 import rospy
 import baxter_interface
-from baxter_interface import gripper as baxter_gripper
+from baxter_interface import gripper
 import pycuber
 from pycuber.solver import CFOPSolver
 from constant_parameters import right_prepare_position, joint_states, Joint_Names, new_joint_states
 import kociemba
+from math import pi
 
 def joint2limb(i):
     right_move = {'right_s0':i[11], 'right_s1':i[12], 'right_e0':i[9], 'right_e1':i[10],
@@ -15,9 +16,22 @@ def joint2limb(i):
                  'left_w0':i[6], 'left_w1':i[7], 'left_w2':i[8]}
     return [right_move, left_move]
 
-# Conversion to motion
-def rotation2motion(rot):
-    r = rospy.Rate(0.2)
+def grip_control(hand, cl_op):
+    #Close = 1, open = 0
+    grip = baxter_gripper.Gripper(hand)
+    gripper.set_holding_force(50)
+    gripper.set_dead_band(1)
+    gripper.set_moving_force(80)
+    if cl_op:
+        gripper.close(block=True)
+        print("Closed ", hand ," gripper!")
+        rospy.sleep(1.0)
+    else:
+        gripper.open(block=True)
+        print("Opened ", hand ," gripper!")
+        rospy.sleep(1.0)
+
+def parse_rot(rot):
     r2m_dict={"U":new_joint_states[2],
               "D":new_joint_states[4],
               "L":new_joint_states[6],
@@ -26,50 +40,37 @@ def rotation2motion(rot):
               "B":new_joint_states[1]
     }
     if "'" in rot:
-        n = 3
-        rot = rot[0]
+        rot_ang = -pi/2
     elif "2" in rot:
-        n = 2
-        rot = rot[0]
+        rot_ang = pi
     else:
-        n = 1
-    limb_r = baxter_interface.Limb("right")
-    limb_l = baxter_interface.Limb("left")
-    left_gripper = baxter_gripper.Gripper('left')
-    right_gripper = baxter_gripper.Gripper('right')
+        rot_ang = pi/2
+    rot = rot[0]
+    return [rot, rot_ang]
+
+def move_limb(hand, move):
+    limb = baxter_interface.Limb(hand)
+    limb.move_to_joint_positions(move)
+    print(hand, "limb moved")
+
+def rotate_side(hand, rot_ang):
+    limb = baxter_interface.Limb(hand)
+    angles = limb.joint_angles()
+    angles['right_w2'] = angles['right_w2'] + rot_ang
+    limb.move_to_joint_positions(angles)
+    print("Side rotated to ", rot_ang)
     
+# Conversion to motion
+def rotation2motion(rot):
+    r = rospy.Rate(0.2)
+    [rot, rot_ang] = parse_rot(rot)
     [right_move, left_move] = joint2limb(r2m_dict[rot])
-    print "Right hand: ", rot                        
-    limb_r.move_to_joint_positions(right_move)
-    gripper = right_gripper
-    gripper.set_holding_force(50)
-    gripper.set_dead_band(1)
-    gripper.set_moving_force(80)
-    if n>=1:
-        gripper.open(block=True)
-        print("Opened Right gripper!")
-        rospy.sleep(1.0)
-    else:
-        gripper.close(block=True)
-        print("Closed Right gripper!")
-        rospy.sleep(1.0)
-    
-    #r.sleep
-    print "Left hand: ", rot
-    limb_l.move_to_joint_positions(left_move)
-    gripper = left_gripper 
-    gripper.set_holding_force(50)
-    gripper.set_dead_band(1)
-    gripper.set_moving_force(80)
-    if n>=1:
-        gripper.open(block=True)
-        print("Opened Left gripper!")
-        rospy.sleep(1.0)
-    else:
-        gripper.close(block=True)
-        print("Closed Left gripper!")
-        rospy.sleep(1.0)
-                            
+    move_limb("left",left_move)
+    move_limb("right", right_move)
+    grip_control("right", 1)
+    rotate_side("right", rot_ang)
+    grip_control("right", 1)
+    print "Waiting for next hand motion:"
     r.sleep
     return
 
@@ -86,6 +87,10 @@ def solution(kubic):
 
 def manipulation(sol):    
     sol = sol.split(" ")
+    print("Put kubic in robot left hand.")
+    input("Press Enter to continue...")
+    grip_control("left", 1)
+    grip_control("right", 0)
     for i in sol:
         rotation2motion(i)
     return 1
